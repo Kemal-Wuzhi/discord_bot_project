9fe42f4bdd96ec461ba435c3dff561391dfde5f1
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-04-26 20:38:45 +0800

    minor fix

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ WORKDIR /app
 
 COPY . /app
 
-RUN apt-get update && apt-get install -y tzdata sqlite3
 RUN pip3 install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y tzdata sqlite3
+
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
 
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
+import subprocess
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+def root(): return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    subprocess.Popen(["python", "discord_cmd.py"])
