7d1240bbcb454ab3816b0dc4a853c846b1a40059
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-04-28 14:25:41 +0800

    add router control

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
+from user import router as user_router
 import subprocess
 
 app = FastAPI()
+app.include_router(user_router)
 
 
-@app.get("/")
-def root(): return {"Hello": "World"}
+# @app.get("/")
+# def root(): return {"Hello": "World"}
 
 
 if __name__ == "__main__":
